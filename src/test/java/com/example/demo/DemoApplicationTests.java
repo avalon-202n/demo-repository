e94f7645f15e416eb8f737f70674edac2c646723
ndy2 <emrdbs12@gmail.com>
2023-02-17 20:32:49 +0900

    style: Indent must use tab characters

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,13 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
-    @Disabled
-    @Test
-    void fail() {
-        throw new RuntimeException("test result publish 실패 확인");
-    }
+	@Disabled
+	@Test
+	void fail() {
+		throw new RuntimeException("test result publish 실패 확인");
+	}
 }

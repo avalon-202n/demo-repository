730f3fc1fdcb89ac6eefe38d5ea6517008f17b01
ndy2 <emrdbs12@gmail.com>
2023-03-08 22:21:45 +0900

    feat: demo gcp sql

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,25 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+interface PersonRepository extends JpaRepository<Person, Long> {
+	List<Person> findAllByName(String name);
+}
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +28,32 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+class HelloController {
+
+	private final PersonRepository personRepository;
+
+	@GetMapping("/hello/{name}")
+	public String hello(@PathVariable String name) {
+		log.info("Hello for : {}", name);
+		Person p = new Person();
+		p.setName(name);
+		personRepository.save(p);
+
+		return "Hello " + name;
+	}
+}
+
+@Entity
+@Data
+class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	String name;
 }
